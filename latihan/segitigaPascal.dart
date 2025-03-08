f3b4d81846d35ee097acb4958e8f4785841ee409
jabiralawfaa <162452844+jabiralawfaa@users.noreply.github.com>
2025-03-08 09:57:42 +0700

    V3

---
+++ latihan/segitigaPascal.dart
@@ -0,0 +1,23 @@
+void main() {
+  int barisan = 5;
+  displayPascalTriangle(barisan);
+}
+
+int pascal(int baris, int kolom) {
+  if (kolom == 0 || kolom == baris) {
+    return 1;
+  }
+  return pascal(baris - 1, kolom - 1) + pascal(baris - 1, kolom);
+}
+
+void displayPascalTriangle(int barisan) {
+  for (int i = 0; i < barisan; i++) {
+    String output = '';
+    for (int j = 0; j <= i; j++) {
+      String pascalStr = pascal(i, j).toString();
+      output += pascalStr;
+    }
+    int padding = barisan - i;
+    print("barisan $i = ${" " * padding} $output");
+  }
+}
